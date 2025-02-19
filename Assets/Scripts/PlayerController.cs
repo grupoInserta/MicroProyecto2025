356f9b510f23e4d128e58c1fe15928f1dbd04557
JaimeSevilla <jaimesevillamoreno1@gmail.com>
2025-02-19 14:34:08 +0100

    Cambios en el controlador de personaje y movimiento del mismo

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,10 +7,15 @@ public class PlayerController : MonoBehaviour
     float v;
     float h;
 
+    public Rigidbody rb;
+    Input tecla;
+    [SerializeField]
+    float velocidad = 5f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        player = new GameObject();
+        //player = new GameObject();
     }
 
     // Update is called once per frame
@@ -19,15 +24,32 @@ public class PlayerController : MonoBehaviour
         Movimiento();
     }
 
+    private void FixedUpdate()
+    {
+        AplicarMovimiento();
+    }
+
     private void Movimiento()
     {
-        h = Input.GetAxis("Horizontal");
-        v = Input.GetAxis("Vertical");
+        h = Input.GetAxisRaw("Horizontal");
+        v = Input.GetAxisRaw("Vertical");
 
+        /*
         if (h >= 0.1)
         {
-            
+            player.transform.position += Vector3.right * velocidad;
         }
+        else if( h <= -0.1)
+        {
+            player.transform.position += Vector3.left * velocidad;
+        }
+            */
     }
+
+    private void AplicarMovimiento()
+    {
+        rb.linearVelocity = new Vector3(h * velocidad, rb.linearVelocity.y, v * velocidad);
+    }
+
 
 }
